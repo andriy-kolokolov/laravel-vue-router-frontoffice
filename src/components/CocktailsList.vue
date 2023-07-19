c60ed355d6ca53563f0214380e2ea2ebdc21864a
SimoneRicco <smnrcc0@gmail.com>
2023-07-19 18:07:58 +0200

    add cocktail list

--- src/components/CocktailsList.vue
+++ src/components/CocktailsList.vue
@@ -1,10 +1,3 @@
-<template>
-  <h2>Cocktails List</h2>
-  <ul>
-    <li v-for="cocktail in arrCocktails" :key="cocktail.id">{{ cocktail.title }}</li>
-  </ul>
-</template>
-
 <script>
 import axios from "axios";
 
@@ -13,15 +6,38 @@ export default {
   data() {
     return {
       arrCocktails: [],
-    }
+    };
+  },
+  methods: {
+    getCocktails() {
+      axios
+        .get("http://127.0.0.1:8000/api/cocktails") // api link
+        .then((response) => (this.arrCocktails = response.data.data));
+    },
   },
   created() {
-    axios.get('') // api link
-        .then(response => console.log(response.data));
-  }
-}
+    this.getCocktails();
+  },
+};
 </script>
 
-<style scoped>
+<template>
+  <h2>Cocktails List</h2>
+  <div
+    class="card"
+    style="width: 18rem"
+    v-for="cocktail in arrCocktails"
+    :key="cocktail.id"
+  >
+    <img :src="cocktail.image" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">{{ cocktail.name }}</h5>
+      <p class="card-text">
+        {{ cocktail.recipe }}
+      </p>
+      <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+    </div>
+  </div>
+</template>
 
-</style>
+<style scoped></style>
