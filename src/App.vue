2193ca8d9d9fea557c6844e6607adef03d8541c9
Andriy-Kolokolov <koloandre22@gmail.com>
2023-07-19 17:38:34 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+
+import {defineComponent} from "vue";
+import CocktailsList from "./components/CocktailsList.vue";
+import AppHeader from "./components/partials/AppHeader.vue";
+import AppFooter from "./components/partials/AppFooter.vue";
+
+export default defineComponent({
+  components: {CocktailsList, AppHeader, AppFooter}
+})
+</script>
+
+<template>
+
+  <app-header/>
+
+  <main>
+    <div class="container">
+      <cocktails-list/>
+    </div>
+  </main>
+
+  <app-footer/>
+
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/CocktailsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>Cocktails List</h2>
+  <ul>
+    <li v-for="cocktail in arrCocktails" :key="cocktail.id">{{ cocktail.title }}</li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ProjectList",
+  data() {
+    return {
+      arrCocktails: [],
+    }
+  },
+  created() {
+    axios.get('') // api link
+        .then(response => console.log(response.data));
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
