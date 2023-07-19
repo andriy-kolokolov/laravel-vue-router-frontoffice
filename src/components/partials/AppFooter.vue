26d3f410752515c49682bfd047c7e52f68e159de
alessandropecchini99 <pecchini99@gmail.com>
2023-07-19 19:11:40 +0200

    push footer

--- src/components/partials/AppFooter.vue
+++ src/components/partials/AppFooter.vue
@@ -6,10 +6,37 @@ export default {
 
 <template>
   <footer class="bg-dark text-white">
-    <div class="container">
-      <a><i class="bi bi-github"></i></a>
+    <div
+      class="content container d-flex align-items-center justify-content-between"
+    >
+      <div class="rights">
+        <span>© 2023 CocktailBar</span>
+      </div>
+
+      <div class="social">
+        <!-- TO DO: FIX HREF -->
+        <a href="linktr.ee/booleangroup5" target="_blank"
+          ><i class="bi bi-github"></i
+        ></a>
+      </div>
     </div>
   </footer>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+footer {
+  .content {
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+
+    a {
+      height: 100%;
+
+      i {
+        font-size: 1.5em;
+        color: white;
+      }
+    }
+  }
+}
+</style>
